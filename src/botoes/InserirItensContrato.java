c088b621d7cf6a39c646590a17cd74ff5d18ccbc
Aliny Kelly <alinykellyfs@gmail.com>
2024-06-27 13:55:53 -0300

    Commit 3 - Ajustes para inserir os itens no contrato.

--- src/botoes/InserirItensContrato.java
+++ src/botoes/InserirItensContrato.java
@@ -3,10 +3,13 @@ package botoes;
 import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
 import br.com.sankhya.extensions.actionbutton.ContextoAcao;
 import br.com.sankhya.extensions.actionbutton.Registro;
+import br.com.sankhya.jape.core.JapeSession;
 import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
 import br.com.sankhya.modelcore.MGEModelException;
 import com.sankhya.ce.jape.JapeHelper;
 
+import javax.management.ObjectName;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.Collection;
@@ -22,37 +25,54 @@ public class InserirItensContrato implements AcaoRotinaJava {
 
             Collection<DynamicVO> itensContratos = JapeHelper.getVOs("AD_IMPORTITECONTPRO", "CODIMPITECONT = " + codImportacao);
             for (DynamicVO vo : itensContratos) {
-                DynamicVO contrato = JapeHelper.getVO("Contrato", "NUMCONTRATO = " + vo.asBigDecimalOrZero("NUMCONTRATO").toString());
-                if (contrato == null) throw new MGEModelException("Contrato não encontrado.");
+                Object codIteImportacao = vo.asBigDecimalOrZero("CODIMPCONTPRO");
 
-                BigDecimal codparc = contrato.asBigDecimalOrZero("CODPARC");
+                DynamicVO contrato = JapeHelper.getVO("Contrato", "AD_CONTRATO = '" + vo.asString("NUMCONTRATO") + "'") ;
 
-                JapeHelper.CreateNewLine newProdServContrato = new JapeHelper.CreateNewLine("ProdutoServicoContrato");
-                newProdServContrato.set("NUMCONTRATO", vo.asBigDecimalOrZero("NUMCONTRATO"));
-                newProdServContrato.set("CODPROD", vo.asBigDecimalOrZero("CODPROD"));
-                newProdServContrato.set("NUMUSUARIOS", vo.asBigDecimalOrZero("NUMUSUARIOS"));
-                newProdServContrato.set("AD_CHAVELPU", vo.asString("CHAVELPU"));
-                newProdServContrato.set("AD_REGIONAL", vo.asString("REGIONAL"));
-                newProdServContrato.save();
+                if (contrato != null) {
+                    BigDecimal nroContrato = contrato.asBigDecimalOrZero("NUMCONTRATO");
+                    BigDecimal codparc = contrato.asBigDecimalOrZero("CODPARC");
 
-                JapeHelper.CreateNewLine newPreco = new JapeHelper.CreateNewLine("PrecoContrato");
-                newPreco.set("NUMCONTRATO", vo.asBigDecimalOrZero("NUMCONTRATO"));
-                newPreco.set("CODPROD", vo.asBigDecimalOrZero("CODPROD"));
-                newPreco.set("REFERENCIA", vo.asTimestamp("REFERENCIA"));
-                newPreco.set("VALOR", vo.asBigDecimalOrZero("VALOR"));
-                newPreco.save();
+                    JapeHelper.CreateNewLine newProdServContrato = new JapeHelper.CreateNewLine("ProdutoServicoContrato");
+                    newProdServContrato.set("NUMCONTRATO", nroContrato);
+                    newProdServContrato.set("CODPROD", vo.asBigDecimalOrZero("CODPROD"));
+                    newProdServContrato.set("NUMUSUARIOS", vo.asBigDecimalOrZero("NUMUSUARIOS"));
+                    newProdServContrato.set("AD_CHAVELPU", vo.asString("CHAVE"));
+                    newProdServContrato.set("AD_REGIONAL", vo.asString("REGIONAL"));
+                    newProdServContrato.save();
 
-                JapeHelper.CreateNewLine newOcorrencia = new JapeHelper.CreateNewLine("OcorrenciaContrato");
-                newOcorrencia.set("NUMCONTRATO", vo.asBigDecimalOrZero("NUMCONTRATO"));
-                newOcorrencia.set("CODPROD", vo.asBigDecimalOrZero("CODPROD"));
-                newOcorrencia.set("DTOCOR", new Timestamp(System.currentTimeMillis()));
-                newOcorrencia.set("CODOCOR", BigDecimal.ONE);
-                newOcorrencia.set("CODCONTATO", BigDecimal.ONE);
-                newOcorrencia.set("CODUSU", BigDecimal.ZERO);
-                newOcorrencia.set("DESCRICAO", "Ativação");
-                newOcorrencia.set("CODPARC", codparc);
-                newOcorrencia.save();
+                    JapeHelper.CreateNewLine newPreco = new JapeHelper.CreateNewLine("PrecoContrato");
+                    newPreco.set("NUMCONTRATO", nroContrato);
+                    newPreco.set("CODPROD", vo.asBigDecimalOrZero("CODPROD"));
+                    newPreco.set("REFERENCIA", vo.asTimestamp("REFERENCIA"));
+                    newPreco.set("VALOR", vo.asBigDecimalOrZero("VALOR"));
+                    newPreco.save();
 
+                    JapeHelper.CreateNewLine newOcorrencia = new JapeHelper.CreateNewLine("OcorrenciaContrato");
+                    newOcorrencia.set("NUMCONTRATO", nroContrato);
+                    newOcorrencia.set("CODPROD", vo.asBigDecimalOrZero("CODPROD"));
+                    newOcorrencia.set("DTOCOR", new Timestamp(System.currentTimeMillis()));
+                    newOcorrencia.set("CODOCOR", BigDecimal.ONE);
+                    newOcorrencia.set("CODCONTATO", BigDecimal.ONE);
+                    newOcorrencia.set("CODUSU", BigDecimal.ZERO);
+                    newOcorrencia.set("DESCRICAO", "Ativação");
+                    newOcorrencia.set("CODPARC", codparc);
+                    newOcorrencia.save();
+                } else {
+                    JapeSession.SessionHandle hnd = null;
+
+                    try {
+                        hnd = JapeSession.open();
+                        JapeFactory.dao("AD_IMPORTITECONTPRO").
+                                prepareToUpdateByPK(codImportacao, codIteImportacao)
+                                .set("ERROR", "Contrato não encontrado.")
+                                .update();
+                    } catch (Exception e) {
+                        MGEModelException.throwMe(e);
+                    } finally {
+                        JapeSession.close(hnd);
+                    }
+                }
             }
         }
 
